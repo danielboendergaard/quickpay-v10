7d0dfd3555529ab70b50f2af589c2853d8523ecb
Daniel Bøndergaard <danielboendergaard@gmail.com>
2015-09-24 13:31:32 +0200

    Add check for required form arguments

--- src/Form.php
+++ src/Form.php
@@ -1,6 +1,8 @@
 <?php
 namespace Kameli\Quickpay;
 
+use InvalidArgumentException;
+
 class Form
 {
     const FORM_ACTION = 'https://payment.quickpay.net';
@@ -8,14 +10,23 @@ class Form
     /**
      * @var array
      */
-    protected $parameters;
+    protected $parameters = [
+        'version' => 'v10',
+    ];
+
+    /**
+     * @var array
+     */
+    protected static $requiredParameters = [
+        'version', 'merchant_id', 'agreement_id', 'order_id', 'amount', 'currency', 'continueurl', 'cancelurl',
+    ];
 
     /**
      * @param array $parameters
      */
     public function __construct(array $parameters)
     {
-        $this->parameters = $parameters;
+        $this->parameters = array_merge($this->parameters, $parameters);
     }
 
     /**
@@ -32,6 +43,13 @@ class Form
      */
     public function render()
     {
+        $missingParameters = array_diff(static::$requiredParameters, array_keys($this->parameters));
+
+        if (! empty($missingParameters)) {
+            $message = 'Missing arguments for Quickpay Form: ' . implode(', ', $missingParameters);
+            throw new InvalidArgumentException($message);
+        }
+
         $fields = [];
         foreach ($this->parameters as $parameter => $value) {
             $fields[] = sprintf('<input type="hidden" name="%s" value="%s">', $parameter, $value);
