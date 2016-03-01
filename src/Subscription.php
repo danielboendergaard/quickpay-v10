fe9aad26b99e51bbee2e4feca30a586209668b13
Daniel Bøndergaard <danielboendergaard@gmail.com>
2016-03-01 11:00:57 +0100

    Work on subscriptions

--- src/Subscription.php
+++ src/Subscription.php
@@ -9,21 +9,40 @@ class Subscription
      */
     protected $client;
 
+    /**
+     * @param \Kameli\Quickpay\Client $client
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * Get all subscriptions
+     * @return array
+     */
     public function all()
     {
         return $this->client->request('GET', '/subscriptions');
     }
 
-    public function create(array $parameters)
+    /**
+     * Create a subscription
+     * Required parameters: order_id, currency, description
+     * @param array $parameters
+     * @return object
+     */
+    public function create($parameters)
     {
         return $this->client->request('POST', '/subscriptions', $parameters);
     }
 
+    /**
+     * Authorize a subscription
+     * @param int $id
+     * @param string $cardToken
+     * @return object
+     */
     public function authorize($id, $cardToken)
     {
         return $this->client->request('POST', "/subscriptions/{$id}/authorize?synchronized", [
@@ -32,8 +51,27 @@ class Subscription
         ]);
     }
 
+    /**
+     * Create a subscription recurring payment
+     * Required parameters: amount, order_id
+     * @param int $id
+     * @param array $parameters
+     * @return object
+     */
     public function recurring($id, $parameters)
     {
         return $this->client->request('POST', "/subscriptions/{$id}/recurring?synchronized", $parameters);
+    }
+
+    /**
+     * Create/update a subscription link
+     * Required parameters: amount
+     * @param int $id
+     * @param array $parameters
+     * @return object
+     */
+    public function link($id, $parameters)
+    {
+        return $this->client->request('PUT', "/subscriptions/{$id}/link", $parameters);
     }
 }
