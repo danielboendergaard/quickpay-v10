c07ea564ab32340686075d5da1903295423c0cf6
Daniel Bøndergaard <danielboendergaard@gmail.com>
2016-03-02 16:34:09 +0100

    Complete the callback class

--- src/Callback.php
+++ src/Callback.php
@@ -9,13 +9,30 @@ class Callback
      */
     protected $client;
 
+    /**
+     * @param \Kameli\Quickpay\Client $client
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * Get failed and queued callbacks
+     * @return array
+     */
     public function all()
     {
         return $this->client->request('GET', '/callbacks');
+    }
+
+    /**
+     * Retry failed callback
+     * @param int $id
+     * @return object
+     */
+    public function retry($id)
+    {
+        return $this->client->request('PATCH', "/callbacks/{$id}/retry");
     }
 }
