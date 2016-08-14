b4c5ee8f944210fecf84f27bc12cde5a26f46f2a
Daniel Bøndergaard <danielboendergaard@gmail.com>
2016-08-14 20:52:17 +0200

    Add autoloader

---
+++ autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    $prefix = 'Kameli\\Quickpay\\';
+    $base_dir = __DIR__ . '/src/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
